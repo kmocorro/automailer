a29c27459358bbfc18aa495e4558e6fd5540e59f
vinx <kevinmocorro@gmail.com>
2017-12-23 17:57:04 +0800

    patch automailer.sh

--- bin/bash/automailer.sh
+++ bin/bash/automailer.sh
@@ -9,7 +9,7 @@
 # varbs
 PATH=$PATH:/c/xampp/mysql/bin
 #checkFile= ../tmp/automailer.txt
-APIRES=$(wget --server-response http://localhost:8000/ -O /c/sandbox/nodejs-automailer/public/reslog.txt 2>&1 | grep -c 'HTTP/1.1 200 OK') #check if my server is online
+APIRES=$(wget --server-response http://localhost:8000/ -O /c/sandbox/nodejs-automailer/public/reslog.txt 2>&1 | grep -c 'HTTP/1.1 200 OK') #check if automailer is online
 HOST="localhost"
 USER="root"
 PASS="2qhls34r"
@@ -19,22 +19,31 @@ POSTP=$(mysql -h$HOST -u $USER -p$PASS $DB -s<<<"SELECT pass FROM tbl_cloud_deta
 
 echo "Checking web service response..."
 
-if [ -e "../tmp/automailer.txt" ]; then
+if [ -e "../tmp/automailer.txt" ]; then # file exists, don't do anythingx
     echo "automailer is already running"
     sleep 5
 else
-    touch ../tmp/automailer.txt
-
+    touch ../tmp/automailer.txt #hoping that bash dosn't crash
+    
     if [ $APIRES != 1 ]; then
         echo "Cannot access API"
-        curl -d "gg=Error404" http://localhost:8000/404    
-        sleep 300
+        curl -d "gg=Error404" http://localhost:8000/404
+
+        if [ "$?" = "7" ]; then
+            echo "Connection refused. Server down"
+            rm ../tmp/automailer.txt
+            sleep 3600 # 1 HOUR WAITING
+        fi
+
+        echo "404. Check server"
         rm ../tmp/automailer.txt
+        sleep 3600 # SAME
+
         sh ./automailer.sh
     else
         echo "Server is active"
         curl -d "user=$POSTU&pass=$POSTP" http://localhost:8000/202
-        sleep 11
+        sleep 5
         rm ../tmp/automailer.txt
         # wait for next trigger 6:30 / 18:30
     fi
